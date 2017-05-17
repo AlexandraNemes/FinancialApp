ace7667bd7ecbd2e12873df2d88f7564038d62d0
AlexandraNemes <alexandra.nemes90@yahoo.com>
2017-05-17 18:14:10 +0300

    - added hashCode and equals methods for FileLine class to be able to compare two lists of FileLine objects

--- src/main/java/financial/file/parser/common/FileLine.java
+++ src/main/java/financial/file/parser/common/FileLine.java
@@ -30,6 +30,47 @@ public class FileLine {
     public String toString() {
 	return "FileLine [lineNumber=" + lineNumber + ", lineContent=" + lineContent + "]";
     }
-}
 
-   
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((lineContent == null) ? 0 : lineContent.hashCode());
+	result = prime * result + lineNumber;
+	return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+	boolean areEqual = true;
+
+	if (this == obj) {
+	    areEqual = true;
+	}
+
+	if (obj == null) {
+	    areEqual = false;
+	} else {
+	    if (getClass() != obj.getClass()) {
+		areEqual = false;
+	    }
+	    FileLine other = (FileLine) obj;
+
+	    if (lineContent == null) {
+		if (other.lineContent != null) {
+		    areEqual = false;
+		}
+	    } else if (!lineContent.equals(other.lineContent)) {
+		areEqual = false;
+	    }
+
+	    if (lineNumber != other.lineNumber) {
+		areEqual = false;
+	    }
+	}
+
+	return areEqual;
+    }
+
+}
